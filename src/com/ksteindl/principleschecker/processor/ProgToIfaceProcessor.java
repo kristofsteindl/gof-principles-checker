d588238dc41e7b34f252f579961d51a645f3e661
Kristóf Steindl <ksteindl@chemaxon.com>
2021-01-26 21:00:27 +0100

    MIDDLE-38 implemented checker business logic

--- src/com/ksteindl/principleschecker/processor/ProgToIfaceProcessor.java
+++ src/com/ksteindl/principleschecker/processor/ProgToIfaceProcessor.java
@@ -11,7 +11,8 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.util.Elements;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
 import javax.tools.Diagnostic.Kind;
 
 import com.ksteindl.principleschecker.annotation.ProgrammedToInterface;
@@ -21,24 +22,38 @@ import com.ksteindl.principleschecker.annotation.ProgrammedToInterface;
 public class ProgToIfaceProcessor extends AbstractProcessor{
 	
 	private Messager messager;
-	private Elements elementUtils;
 	
 	@Override
 	public synchronized void init(ProcessingEnvironment processingEnv) {
 		super.init(processingEnv);
 		this.messager = processingEnv.getMessager();
-		this.elementUtils = processingEnv.getElementUtils();
 	}
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(ProgrammedToInterface.class);
-		for (Element element : elements) {
-			if (element.getKind().isClass()) {
-				messager.printMessage(Kind.ERROR, "GOF1 - Programmed to Interface violated", element);
-			}
-		}	
+		roundEnv.getElementsAnnotatedWith(ProgrammedToInterface.class)
+				.stream()
+				.filter(element -> element.getKind().isClass())
+				.flatMap(element -> element.getEnclosedElements().stream())
+				.filter(fieldELement -> fieldELement.getKind().isField())
+				.filter(fieldELement -> !fieldELement.asType().getKind().isPrimitive())
+				.filter(fieldELement -> !fieldELement.asType().toString().equals("java.lang.String"))
+				.filter(fieldELement -> !fieldIsInterface(fieldELement))
+				.forEach(fieldELement -> messager.printMessage(Kind.ERROR, "This field (" 
+						+ fieldELement.asType().toString() + ") violates GOF1: "
+						+ "\"programmed to interface\" principle. Use only primitive type or interface"
+						, fieldELement));
 		return true;
 	}
+	
+	private boolean fieldIsInterface(Element fieldElement) {
+		if (fieldElement.asType().getKind() != TypeKind.DECLARED) {
+			return false;
+		}
+		DeclaredType declaredFieldType = (DeclaredType) fieldElement.asType();
+		TypeElement fieldTypeElement = (TypeElement) declaredFieldType.asElement();
+		return fieldTypeElement.getKind().isInterface();
+	}
+
 
 }
